af9799aa855290f47762759fb6fff74ced8cbae5
Jason Priddy <jason_priddy@yahoo.com>
2016-03-02 19:08:27 -0500

    committing angularJS custom directive/routing code

--- src/TheWorld/wwwroot/js/app-trips.js
+++ src/TheWorld/wwwroot/js/app-trips.js
@@ -4,6 +4,23 @@
   "use strict";
 
   //creating the module
-  angular.module("app-trips", []);
+  angular.module("app-trips", ["simpleControls", "ngRoute"]).config(function ($routeProvider) {
+
+      $routeProvider.when("/", {
+          controller: "tripsController",
+          controllerAs: "vm",
+          templateUrl: "/views/tripsView.html"
+      });
+
+      $routeProvider.when("/editor/:tripName", {
+          controller: "tripEditorController",
+          controllerAs: "vm",
+          templateUrl: "/views/tripEditorView.html"
+      });
+
+
+      $routeProvider.otherwise({ redirectTo: "/" });
+
+  });
 
 })();
